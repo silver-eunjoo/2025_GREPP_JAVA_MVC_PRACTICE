9299b0012a53add35e64e3070d4dc20999f0f01f
silver-eunjoo <leeeun5423@naver.com>
2025-03-22 22:28:04 +0900

    UriParser 수정

--- src/main/java/io/silver/sys/UriParser.java
+++ src/main/java/io/silver/sys/UriParser.java
@@ -2,7 +2,6 @@ package io.silver.sys;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 public class UriParser {
 
@@ -27,42 +26,53 @@ public class UriParser {
             return uri;
         }
 
-        try{
-            String[] split = uri.split("//?", 2);
 
-            if(split.length == 2) { // 파라미터가 있을 때
-                setParameters(split[1]);
-                return uri;
-            }
+        String[] uriSplit = uri.split("\\?", 2);
 
-            // 파라미터가 없을 때
-            String[] uriFront = split[0].split("/");
-
-            // ["", "posts", "add"]
-            if(uriFront.length != 3) {
-                throw new IllegalArgumentException("uri가 잘못되었습니다. 다시 입력해주세요");
-            }
-
-            controllerCode = uriFront[1];
-            function = uriFront[2];
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("잘못된 URL 형식입니다. 다시 입력해주시기 바랍니다.");
+        if(uriSplit.length == 2) { // 파라미터가 있을 때
+            setParameters(uriSplit[1]);
         }
+
+        // 파라미터가 없을 때
+        String[] uriFront = uriSplit[0].split("/");
+
+        // ["", "posts", "add"]
+        if(uriFront.length != 3) {
+            isValid = false;
+            return uri;
+        }
+
+        controllerCode = uriFront[1];
+        function = uriFront[2];
 
         return uri;
     }
 
     private void setParameters(String uriBack) {
         //"param1=value1&param2=value"
-        if(uriBack.contains("&")){ //파라미터가 여러가지 일 때
-            String[] split = uriBack.split("&");
-            for (String s : split) {
-                String[] params = s.split("=");
-                parameter.put(params[0], params[1]);
+        try{
+            if(uriBack.contains("&")){ //파라미터가 여러가지 일 때
+                String[] split = uriBack.split("&");
+                for (String s : split) {
+                    String[] params = s.split("=",2);
+
+                    if(params[1].equals("")) {
+                        throw new IllegalArgumentException("잘못된 파라미터 값이 입력되었습니다.URL을 확인해주세요.");
+                    }
+
+                    parameter.put(params[0], params[1]);
+                }
+            } else {
+                String[] split = uriBack.split("=", 2);
+
+                if(split[1].equals("")) {
+                    throw new IllegalArgumentException("잘못된 파라미터 값이 입력되었습니다.URL을 확인해주세요.");
+                }
+
+                parameter.put(split[0], split[1]);
             }
-        } else {
-            String[] split = uriBack.split("=", 2);
-            parameter.put(split[0], split[1]);
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            this.isValid = false;
         }
     }
 
@@ -76,10 +86,6 @@ public class UriParser {
 
     public String getFunction() {
         return function;
-    }
-
-    public String getURI() {
-        return URI;
     }
 
     public Map<String, Object> getParameter() {
