e5dabfd67e4b5bf3acf028eda0892a15eab9cc9e
silver-eunjoo <leeeun5423@naver.com>
2025-03-22 22:29:56 +0900

    Request 수정

--- src/main/java/io/silver/sys/Request.java
+++ src/main/java/io/silver/sys/Request.java
@@ -1,15 +1,28 @@
 package io.silver.sys;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Request {
 
     private UriParser parser;
+    private Map<String, Object> parameters = new HashMap<>();
 
     public Request(String command) {
         parser = new UriParser(command);
+        parameters = this.parser.getParameter();
     }
 
     public boolean isValid() {
         return this.parser.isValid();
+    }
+
+    public String getControllerCode() {
+        return this.parser.getControllerCode();
+    }
+
+    public String getFunction() {
+        return this.parser.getFunction();
     }
 
 }
